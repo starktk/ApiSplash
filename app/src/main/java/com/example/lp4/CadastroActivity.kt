a82378c9805b1437a0d721165863caf856e99884
starktk <raulsurf.rodrigues@gmail.com>
2023-03-29 17:32:52 -0300

    teste

--- app/src/main/java/com/example/lp4/CadastroActivity.kt
+++ app/src/main/java/com/example/lp4/CadastroActivity.kt
@@ -1,10 +1,12 @@
 package com.example.lp4
 
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.fundatecti21.R
 
-class CadastroActivity: : AppCompatActivity(){
+class CadastroActivity: AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_loginprincipal)
+    }
 }
--- app/src/main/java/com/example/lp4/SplashScreenLogo.kt
+++ app/src/main/java/com/example/lp4/SplashScreenLogo.kt
@@ -7,7 +7,7 @@ import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import com.example.fundatecti21.R
 
-class SplashScreenLayout : AppCompatActivity(){
+class SplashScreenLogo : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_logo)
